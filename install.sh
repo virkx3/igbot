82d433ddfac5cdf0ac285d3a49b172fcbe705450
Fahmi Dev <52388234+Ainx-BOT@users.noreply.github.com>
2021-12-17 14:08:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ pkg install x11-repo -y
 pkg update && pkg upgrade -y
 pkg install git python -y -y
 termux-setup-storage -y
-git clone https://github.com/Ainx-BOT/igbot
-cd igbot
-pip install --upgrade pip
-pip install -r requirements.txt
-python run.py
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 run.py
